8471416ccfcca9d203b1868a25919dd66451e8bf
1-201212767 <eunsuck93@gmail.com>
2017-05-31 22:10:24 +0900

    첫 페이지 추가, 기본적인 툴바메뉴 작성(클릭 이벤트는 아직 만들지 않음)

---
+++ app/src/main/java/psykehg/istheredust/Main2Activity.java
@@ -0,0 +1,27 @@
+package psykehg.istheredust;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
